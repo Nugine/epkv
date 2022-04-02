c5f5dd0076befd74bd375fc0dfad0baace1b4e0d
Nugine <nugine@foxmail.com>
2022-04-02 23:32:56 +0800

    utils: init

---
+++ crates/epkv-utils/src/lib.rs
@@ -0,0 +1,9 @@
+#![deny(
+    unsafe_code,
+    clippy::all,
+    clippy::as_conversions,
+    clippy::float_arithmetic,
+    clippy::integer_arithmetic,
+    clippy::must_use_candidate,
+    clippy::missing_inline_in_public_items
+)]
