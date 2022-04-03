70c8444d763535ca915c0486f53a6402007d19d0
Nugine <nugine@foxmail.com>
2022-04-03 20:49:31 +0800

    epaxos: codec

---
+++ crates/epkv-epaxos/src/codec.rs
@@ -0,0 +1,29 @@
+use std::error::Error;
+
+use bincode::Options;
+use bytes::Bytes;
+use serde::de::DeserializeOwned;
+use serde::Serialize;
+
+pub fn serialize<T>(value: &T) -> Result<Bytes, impl Error>
+where
+    T: Serialize,
+{
+    bincode::DefaultOptions::new()
+        .serialize(value)
+        .map(Bytes::from)
+}
+
+pub fn deserialize_owned<T>(bytes: &[u8]) -> Result<T, impl Error>
+where
+    T: DeserializeOwned,
+{
+    bincode::DefaultOptions::new().deserialize(bytes)
+}
+
+pub fn serialized_size<T>(value: &T) -> Result<u64, impl Error>
+where
+    T: Serialize,
+{
+    bincode::DefaultOptions::new().serialized_size(value)
+}
