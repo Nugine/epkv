0fd92f615f51406c75b9653a981652a47c4041ca
Nugine <nugine@foxmail.com>
2022-04-05 15:47:58 +0800

    epaxos: acc

---
+++ crates/epkv-epaxos/src/acc.rs
@@ -0,0 +1,22 @@
+use crate::id::ReplicaId;
+
+use epkv_utils::vecset::VecSet;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub struct Acc(VecSet<ReplicaId>);
+
+impl Acc {
+    pub fn with_capacity(cap: usize) -> Self {
+        Self(VecSet::with_capacity(cap))
+    }
+
+    pub fn insert(&mut self, rid: ReplicaId) {
+        let _ = self.0.insert(rid);
+    }
+
+    pub fn union(&mut self, other: &Self) {
+        self.0.union_copied(&other.0);
+    }
+}
--- crates/epkv-epaxos/src/lib.rs
+++ crates/epkv-epaxos/src/lib.rs
@@ -1,6 +1,7 @@
 #![forbid(unsafe_code)]
 #![deny(clippy::all)]
 
+pub mod acc;
 pub mod cmd;
 pub mod deps;
 pub mod id;
