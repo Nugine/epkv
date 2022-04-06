d8dbfc9589b44c335db1bb1049232580913f9150
Nugine <nugine@foxmail.com>
2022-04-06 20:57:08 +0800

    epaxos: store: LogStore

--- crates/epkv-epaxos/src/types/store.rs
+++ crates/epkv-epaxos/src/types/store.rs
@@ -1,11 +1,15 @@
 use super::id::{Ballot, InstanceId};
 use super::ins::Instance;
+use super::{CommandLike, Deps, Seq};
 
 use anyhow::Result;
 
-pub trait LogStore<C>: Send + Sync + 'static {
-    fn load_instance(&mut self, id: InstanceId) -> Result<Option<Instance<C>>>;
-    fn save_instance(&mut self, id: InstanceId, instance: Instance<C>) -> Result<()>;
-    fn load_pbal(&mut self, id: InstanceId) -> Result<Option<Ballot>>;
-    fn save_pbal(&mut self, id: InstanceId, pbal: Ballot) -> Result<()>;
+pub trait LogStore: Send + Sync + 'static {
+    type Command: CommandLike;
+    fn load_instance(&self, id: InstanceId) -> Result<Option<Instance<Self::Command>>>;
+    fn save_instance(&self, id: InstanceId, instance: Instance<Self::Command>) -> Result<()>;
+    fn load_pbal(&self, id: InstanceId) -> Result<Option<Ballot>>;
+    fn save_pbal(&self, id: InstanceId, pbal: Ballot) -> Result<()>;
+    fn calc_deps(&self, id: InstanceId, cmd: &Self::Command) -> Result<Deps>;
+    fn calc_seq(&self, id: InstanceId, cmd: &Self::Command) -> Result<Seq>;
 }
