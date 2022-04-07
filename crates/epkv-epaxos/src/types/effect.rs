7801617d42979bbf995fb27de782861c9472483c
Nugine <nugine@foxmail.com>
2022-04-07 10:28:03 +0800

    epaxos: types: effect

--- crates/epkv-epaxos/src/types/effect.rs
+++ crates/epkv-epaxos/src/types/effect.rs
@@ -19,11 +19,20 @@ pub struct Reply<C> {
 }
 
 impl<C: CommandLike> Effect<C> {
+    #[must_use]
+    pub fn empty() -> Self {
+        Effect {
+            broadcasts: Vec::new(),
+            replies: Vec::new(),
+            notifies: Vec::new(),
+        }
+    }
+
+    #[must_use]
     pub fn broadcast(targets: Vec<ReplicaId>, msg: Message<C>) -> Self {
         Effect {
             broadcasts: vec![Broadcast { targets, msg }],
-            replies: Vec::new(),
-            notifies: Vec::new(),
+            ..Self::empty()
         }
     }
 }
