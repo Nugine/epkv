eed1e551f0b56d89de21c70c3e5b567ed1ae753b
Nugine <nugine@foxmail.com>
2022-04-07 22:24:31 +0800

    epaxos: replica: space

--- crates/epkv-epaxos/src/replica/space.rs
+++ crates/epkv-epaxos/src/replica/space.rs
@@ -1,5 +1,6 @@
 use crate::types::*;
 
+use epkv_utils::cmp::max_assign;
 use epkv_utils::vecmap::VecMap;
 
 use std::collections::HashMap;
@@ -76,5 +77,41 @@ impl<S: LogStore> Space<S> {
         let lid = self.max_lid.add_one();
         self.max_lid = lid;
         lid
+    }
+
+    pub fn calc_attributes(&self, id: InstanceId, keys: &Keys<S::Command>) -> (Seq, Deps) {
+        let mut deps = Deps::with_capacity(self.max_lid_map.len());
+        let mut seq = Seq::ZERO;
+        let InstanceId(rid, lid) = id;
+
+        match keys {
+            Keys::Bounded(ref keys) => {
+                let conflicts: _ = keys.iter().filter_map(|k: _| self.max_key_map.get(k));
+                for m in conflicts {
+                    let others: _ = m.lids.iter().copied().filter(|&(r, _)| r != rid);
+                    for (r, l) in others {
+                        deps.insert(InstanceId(r, l));
+                    }
+                    max_assign(&mut seq, m.seq);
+                }
+                let others: _ = self.max_lid_map.iter().filter(|(r, _)| *r != rid);
+                for &(r, ref m) in others {
+                    deps.insert(InstanceId(r, m.checkpoint));
+                }
+                max_assign(&mut seq, self.max_seq.checkpoint);
+            }
+            Keys::Unbounded => {
+                let others: _ = self.max_lid_map.iter().filter(|(r, _)| *r != rid);
+                for &(r, ref m) in others {
+                    deps.insert(InstanceId(r, m.any));
+                }
+                max_assign(&mut seq, self.max_seq.any);
+            }
+        }
+        if lid > LocalInstanceId::ONE {
+            deps.insert(InstanceId(rid, lid.sub_one()));
+        }
+        seq = seq.add_one();
+        (seq, deps)
     }
 }
