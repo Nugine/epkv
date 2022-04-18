b9fef6b19c3a901d97845f5e7836bcd487270a73
Nugine <nugine@foxmail.com>
2022-04-18 15:36:46 +0800

    utils: map_collect

--- crates/epkv-utils/src/iter.rs
+++ crates/epkv-utils/src/iter.rs
@@ -9,3 +9,11 @@ where
 {
     iter.into_iter().copied().map(f).collect()
 }
+
+#[inline]
+pub fn map_collect<T, U, C>(iter: impl IntoIterator<Item = T>, f: impl FnMut(T) -> U) -> C
+where
+    C: FromIterator<U>,
+{
+    iter.into_iter().map(f).collect()
+}
