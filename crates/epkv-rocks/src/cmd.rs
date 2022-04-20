b3654cbabe30c72c6aa9ebacbcb612c97ef66e4d
Nugine <nugine@foxmail.com>
2022-04-20 23:27:03 +0800

    rocks: cmd: epaxos

--- crates/epkv-rocks/src/cmd.rs
+++ crates/epkv-rocks/src/cmd.rs
@@ -15,6 +15,10 @@ impl BatchedCommand {
     pub fn from_vec(v: Vec<Command>) -> Self {
         Self(Arc::from(v))
     }
+
+    fn as_slice(&self) -> &[Command] {
+        &*self.0
+    }
 }
 
 impl<'de> Deserialize<'de> for BatchedCommand {
@@ -41,6 +45,10 @@ impl Serialize for BatchedCommand {
 pub struct Command(Asc<MutableCommand>);
 
 impl Command {
+    fn as_inner(&self) -> &MutableCommand {
+        &*self.0
+    }
+
     pub fn from_mutable(cmd: MutableCommand) -> Self {
         Self(Asc::new(cmd))
     }
@@ -54,9 +62,33 @@ impl Command {
     }
 }
 
+impl AsRef<MutableCommand> for Command {
+    fn as_ref(&self) -> &MutableCommand {
+        self.as_inner()
+    }
+}
+
 #[derive(Clone, Serialize, Deserialize)]
 pub struct MutableCommand {
     pub kind: CommandKind,
+}
+
+impl MutableCommand {
+    fn is_fence(&self) -> bool {
+        matches!(self.kind, CommandKind::Fence(_))
+    }
+
+    fn is_nop(&self) -> bool {
+        matches!(self.kind, CommandKind::Nop(_))
+    }
+
+    fn create_nop() -> Self {
+        Self { kind: CommandKind::Nop(Nop {}) }
+    }
+
+    fn create_fence() -> Self {
+        Self { kind: CommandKind::Fence(Fence {}) }
+    }
 }
 
 #[derive(Clone, Serialize, Deserialize)]
@@ -90,6 +122,58 @@ pub struct Nop {}
 
 #[derive(Clone, Serialize, Deserialize)]
 pub struct Fence {}
+
+mod epaxos {
+    use super::*;
+
+    use epkv_epaxos::cmd::{CommandLike, Keys};
+    use epkv_utils::vecset::VecSet;
+
+    impl CommandLike for BatchedCommand {
+        type Key = Key;
+
+        fn keys(&self) -> Keys<Self> {
+            let this = self.as_slice();
+            let is_fence = this.iter().all(|c| c.as_inner().is_fence());
+            if is_fence {
+                return Keys::Unbounded;
+            }
+            let mut keys = Vec::with_capacity(this.len());
+            for c in this {
+                match c.as_inner().kind {
+                    CommandKind::Get(ref c) => keys.push(c.key.clone()),
+                    CommandKind::Set(ref c) => keys.push(c.key.clone()),
+                    CommandKind::Del(ref c) => keys.push(c.key.clone()),
+                    CommandKind::Nop(_) => {}
+                    CommandKind::Fence(_) => {}
+                }
+            }
+            Keys::Bounded(VecSet::from_vec(keys))
+        }
+
+        fn is_nop(&self) -> bool {
+            self.as_slice().iter().all(|c| c.as_inner().is_nop())
+        }
+
+        fn create_nop() -> Self {
+            let nop = Command::from_mutable(MutableCommand::create_nop());
+            Self::from_vec(vec![nop])
+        }
+
+        fn create_fence() -> Self {
+            let fence = Command::from_mutable(MutableCommand::create_fence());
+            Self::from_vec(vec![fence])
+        }
+
+        fn notify_committed(&self) {
+            todo!()
+        }
+
+        fn notify_executed(&self) {
+            todo!()
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
