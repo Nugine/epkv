d6863129f573940bd80719b5e66f5081cb8b33b5
Nugine <nugine@foxmail.com>
2022-04-21 21:44:19 +0800

    rocks: data_db: impl DataStore

--- crates/epkv-rocks/src/data_db.rs
+++ crates/epkv-rocks/src/data_db.rs
@@ -1,12 +1,18 @@
 //! <https://github.com/tikv/rust-rocksdb>
 
-use crate::cmd::{CommandKind, Del, Get, Set};
+use crate::cmd::{BatchedCommand, CommandKind, Del, Get, Set};
 use crate::error::RocksDbErrorExt;
 use crate::kv::BytesValue;
+
+use epkv_epaxos::exec::ExecNotify;
+use epkv_epaxos::id::InstanceId;
+use epkv_epaxos::store::DataStore;
+use epkv_utils::asc::Asc;
 
 use std::sync::Arc;
 
 use anyhow::Result;
+use async_trait::async_trait;
 use camino::Utf8Path;
 use rocksdb::{Writable, DB};
 
@@ -47,5 +53,35 @@ impl DataDb {
     pub fn execute_del(self: &Arc<Self>, cmd: Del) -> Result<()> {
         self.db.delete(cmd.key.as_ref()).cvt()?;
         Ok(())
+    }
+
+    pub fn batched_execute(self: &Arc<Self>, cmd: BatchedCommand) -> Result<()> {
+        for cmd in cmd.as_slice() {
+            let cmd = cmd.as_ref();
+            let kind = cmd.kind.clone();
+            self.execute(kind)?;
+            if let Some(ref n) = cmd.notify {
+                n.notify_executed();
+            }
+        }
+        Ok(())
+    }
+}
+
+#[async_trait]
+impl DataStore<BatchedCommand> for Arc<DataDb> {
+    async fn issue(
+        &self,
+        _: InstanceId,
+        cmd: BatchedCommand,
+        notify: Asc<ExecNotify>,
+    ) -> Result<()> {
+        let this = Arc::clone(self);
+        let task = move || {
+            let result = this.batched_execute(cmd);
+            notify.notify_executed();
+            result
+        };
+        tokio::task::spawn_blocking(task).await.unwrap()
     }
 }
