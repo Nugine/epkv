68d03dde05b63297afdce00734dcc1bf2cae7c00
Nugine <nugine@foxmail.com>
2022-04-23 15:03:07 +0800

    rocks: log_db

--- crates/epkv-rocks/src/lib.rs
+++ crates/epkv-rocks/src/lib.rs
@@ -34,5 +34,5 @@ pub mod db_utils;
 
 pub mod data_db;
 
+pub mod log_db;
 pub mod log_key;
-// pub mod log_db;
--- crates/epkv-rocks/src/log_db.rs
+++ crates/epkv-rocks/src/log_db.rs
@@ -1,10 +1,10 @@
 //! <https://github.com/tikv/rust-rocksdb>
 
 use crate::cmd::BatchedCommand;
-use crate::error::RocksDbErrorExt;
+use crate::db_utils::{get_value, put_small_value, put_value};
 use crate::log_key::{GlobalFieldKey, InstanceFieldKey};
 
-use epkv_epaxos::bounds::{AttrBounds, StatusBounds};
+use epkv_epaxos::bounds::{AttrBounds, StatusBounds, StatusMap};
 use epkv_epaxos::deps::Deps;
 use epkv_epaxos::id::{Ballot, InstanceId, LocalInstanceId, ReplicaId, Seq};
 use epkv_epaxos::ins::Instance;
@@ -12,10 +12,10 @@ use epkv_epaxos::status::Status;
 use epkv_epaxos::store::UpdateMode;
 
 use epkv_utils::codec;
+use epkv_utils::onemap::OneMap;
 use epkv_utils::vecmap::VecMap;
 use epkv_utils::vecset::VecSet;
 
-use std::io;
 use std::ops::Not;
 use std::sync::Arc;
 
@@ -23,8 +23,7 @@ use anyhow::{ensure, Result};
 use bytemuck::bytes_of;
 use bytemuck::checked::{from_bytes, try_from_bytes};
 use camino::Utf8Path;
-use rocksdb::{WriteBatch, DB};
-use serde::de::DeserializeOwned;
+use rocksdb::{DBRawIterator, WriteBatch, DB};
 use serde::{Deserialize, Serialize};
 use tracing::debug;
 
@@ -32,35 +31,7 @@ pub struct LogDb {
     db: DB,
 }
 
-fn put_small_value<T: Serialize>(db: &impl Writable, key: &[u8], value: &T) -> Result<()> {
-    let mut buf = [0u8; 64];
-    let pos: usize = {
-        let mut cursor = io::Cursor::new(buf.as_mut_slice());
-        codec::serialize_into(&mut cursor, &value)?;
-        cursor.position().try_into().unwrap()
-    };
-    db.put(key, &buf[..pos]).cvt()?;
-    Ok(())
-}
-
-fn put_value<T>(db: &impl Writable, key: &[u8], buf: &mut Vec<u8>, value: &T) -> Result<()>
-where
-    T: Serialize,
-{
-    buf.clear();
-    codec::serialize_into(&mut *buf, value)?;
-    db.put(key, buf.as_slice()).cvt()?;
-    Ok(())
-}
-
-fn get_value<T: DeserializeOwned>(db: &DB, key: &[u8]) -> Result<Option<T>> {
-    match db.get(key).cvt()? {
-        None => Ok(None),
-        Some(v) => Ok(Some(codec::deserialize_owned(&*v)?)),
-    }
-}
-
-#[derive(Deserialize, Serialize)]
+#[derive(Default, Deserialize, Serialize)]
 pub struct SavedStatusBounds {
     pub known_up_to: VecMap<ReplicaId, LocalInstanceId>,
     pub committed_up_to: VecMap<ReplicaId, LocalInstanceId>,
@@ -69,7 +40,7 @@ pub struct SavedStatusBounds {
 
 impl LogDb {
     pub fn new(path: &Utf8Path) -> Result<Arc<Self>> {
-        let db = DB::open_default(path.as_ref()).cvt()?;
+        let db = DB::open_default(path)?;
         Ok(Arc::new(Self { db }))
     }
 
@@ -84,36 +55,36 @@ impl LogDb {
             UpdateMode::Partial => false,
         };
 
-        let wb = WriteBatch::new();
+        let mut wb = WriteBatch::default();
 
         let mut log_key = InstanceFieldKey::new(id, 0);
         let mut buf = Vec::new();
 
         if needs_save_cmd {
             log_key.set_field(InstanceFieldKey::FIELD_CMD);
-            put_value(&wb, bytes_of(&log_key), &mut buf, &ins.cmd)?;
+            put_value(&mut wb, bytes_of(&log_key), &mut buf, &ins.cmd)?;
         }
 
         // pbal
         {
             log_key.set_field(InstanceFieldKey::FIELD_PBAL);
-            put_value(&wb, bytes_of(&log_key), &mut buf, &ins.pbal)?;
+            put_value(&mut wb, bytes_of(&log_key), &mut buf, &ins.pbal)?;
         }
 
         // status
         {
             log_key.set_field(InstanceFieldKey::FIELD_STATUS);
-            put_value(&wb, bytes_of(&log_key), &mut buf, &ins.status)?;
+            put_value(&mut wb, bytes_of(&log_key), &mut buf, &ins.status)?;
         }
 
         // (seq, deps, abal, acc)
         {
             log_key.set_field(InstanceFieldKey::FIELD_OTHERS);
             let value: _ = (ins.seq, &ins.deps, ins.abal, &ins.acc);
-            put_value(&wb, bytes_of(&log_key), &mut buf, &value)?;
+            put_value(&mut wb, bytes_of(&log_key), &mut buf, &value)?;
         }
 
-        self.db.write(&wb).cvt()?;
+        self.db.write(wb)?;
 
         Ok(())
     }
@@ -122,39 +93,46 @@ impl LogDb {
         // <https://github.com/facebook/rocksdb/wiki/Basic-Operations#iteration>
         // <https://github.com/facebook/rocksdb/wiki/Iterator>
 
-        let mut iter = self.db.iter();
+        let mut iter = self.db.raw_iterator();
 
         let cmd: BatchedCommand = {
             let log_key = InstanceFieldKey::new(id, InstanceFieldKey::FIELD_CMD);
-            let is_valid = iter.seek(SeekKey::Key(bytes_of(&log_key))).cvt()?;
-            if is_valid.not() {
+
+            iter.seek(bytes_of(&log_key));
+
+            if iter.valid().not() {
+                iter.status()?;
                 debug!(?id, "not found");
                 return Ok(None);
             }
 
-            let log_key: &InstanceFieldKey = match try_from_bytes(iter.key()) {
+            let log_key: &InstanceFieldKey = match try_from_bytes(iter.key().unwrap()) {
                 Ok(k) => k,
                 Err(_) => {
                     debug!(?id, iter_key = ?iter.key());
                     return Ok(None);
                 }
             };
+
             if log_key.id() != id {
                 return Ok(None);
             }
             if log_key.field() != InstanceFieldKey::FIELD_CMD {
                 return Ok(None);
             }
-            codec::deserialize_owned(iter.value())?
+
+            codec::deserialize_owned(iter.value().unwrap())?
         };
 
         macro_rules! next_field {
             ($field:tt) => {{
-                ensure!(iter.next().cvt()?);
-                let log_key: &InstanceFieldKey = from_bytes(iter.key());
+                iter.next();
+                iter.status()?;
+                ensure!(iter.valid());
+                let log_key: &InstanceFieldKey = from_bytes(iter.key().unwrap());
                 assert_eq!(log_key.id(), id);
                 assert_eq!(log_key.field(), InstanceFieldKey::$field);
-                codec::deserialize_owned(iter.value())?
+                codec::deserialize_owned(iter.value().unwrap())?
             }};
         }
 
@@ -170,7 +148,7 @@ impl LogDb {
 
     pub fn save_pbal(self: &Arc<Self>, id: InstanceId, pbal: Ballot) -> Result<()> {
         let log_key = InstanceFieldKey::new(id, InstanceFieldKey::FIELD_PBAL);
-        put_small_value(&self.db, bytes_of(&log_key), &pbal)
+        put_small_value(&mut &self.db, bytes_of(&log_key), &pbal)
     }
 
     pub fn load_pbal(self: &Arc<Self>, id: InstanceId) -> Result<Option<Ballot>> {
@@ -180,25 +158,125 @@ impl LogDb {
 
     pub fn update_status(self: &Arc<Self>, id: InstanceId, status: Status) -> Result<()> {
         let log_key = InstanceFieldKey::new(id, InstanceFieldKey::FIELD_STATUS);
-        put_small_value(&self.db, bytes_of(&log_key), &status)
+        put_small_value(&mut &self.db, bytes_of(&log_key), &status)
     }
 
     pub fn save_bounds(self: &Arc<Self>, attr: AttrBounds, status: SavedStatusBounds) -> Result<()> {
         let mut buf = Vec::new();
-        let wb = WriteBatch::new();
+        let mut wb = WriteBatch::default();
         {
             let log_key = GlobalFieldKey::new(GlobalFieldKey::FIELD_ATTR_BOUNDS);
-            put_value(&wb, bytes_of(&log_key), &mut buf, &attr)?;
+            put_value(&mut wb, bytes_of(&log_key), &mut buf, &attr)?;
         }
         {
             let log_key = GlobalFieldKey::new(GlobalFieldKey::FIELD_STATUS_BOUNDS);
-            put_value(&wb, bytes_of(&log_key), &mut buf, &status)?;
+            put_value(&mut wb, bytes_of(&log_key), &mut buf, &status)?;
         }
-        self.db.write(&wb).cvt()?;
+        self.db.write(wb)?;
         Ok(())
     }
 
+    fn load_bounds_optional(
+        &self,
+        iter: &mut DBRawIterator<'_>,
+    ) -> Result<Option<(AttrBounds, SavedStatusBounds)>> {
+        let log_key = GlobalFieldKey::new(GlobalFieldKey::FIELD_ATTR_BOUNDS);
+        iter.seek(bytes_of(&log_key));
+
+        if iter.valid().not() {
+            iter.status()?;
+            return Ok(None);
+        }
+
+        if bytes_of(&log_key) != iter.key().unwrap() {
+            return Ok(None);
+        }
+
+        let attr_bounds: AttrBounds = codec::deserialize_owned(iter.value().unwrap())?;
+
+        let log_key = GlobalFieldKey::new(GlobalFieldKey::FIELD_STATUS_BOUNDS);
+        iter.seek(bytes_of(&log_key));
+        iter.status()?;
+        ensure!(iter.valid());
+        assert_eq!(bytes_of(&log_key), iter.key().unwrap());
+        let saved_status_bounds: SavedStatusBounds = codec::deserialize_owned(iter.value().unwrap())?;
+
+        Ok(Some((attr_bounds, saved_status_bounds)))
+    }
+
     pub fn load_bounds(self: &Arc<Self>) -> Result<(AttrBounds, StatusBounds)> {
+        let mut iter = self.db.raw_iterator();
+
+        let (attr_bounds, saved_status_bounds) = self.load_bounds_optional(&mut iter)?.unwrap_or_else(|| {
+            let attr_bounds = AttrBounds { max_seq: Seq::ZERO, max_lids: VecMap::new() };
+            let saved_status_bounds = SavedStatusBounds::default();
+            (attr_bounds, saved_status_bounds)
+        });
+
+        let status_bounds = {
+            let mut maps: VecMap<ReplicaId, StatusMap> = VecMap::new();
+
+            let create_default = || StatusMap {
+                known: OneMap::new(0),
+                committed: OneMap::new(0),
+                executed: OneMap::new(0),
+            };
+
+            let mut merge: _ = |map: &VecMap<ReplicaId, LocalInstanceId>,
+                                project: fn(&mut StatusMap) -> &mut OneMap| {
+                for &(rid, lid) in map {
+                    let (_, m) = maps.init_with(rid, create_default);
+                    ((project)(m)).set_bound(lid.raw_value());
+                }
+            };
+
+            merge(&saved_status_bounds.known_up_to, |m| &mut m.known);
+            merge(&saved_status_bounds.committed_up_to, |m| &mut m.committed);
+            merge(&saved_status_bounds.executed_up_to, |m| &mut m.executed);
+
+            StatusBounds::from_maps(maps)
+        };
+
+        {
+            
+            let field_status = InstanceFieldKey::FIELD_STATUS;
+            let zero_id = InstanceId(ReplicaId::ONE, LocalInstanceId::ZERO);
+            let mut log_key = InstanceFieldKey::new(zero_id, field_status);
+
+            // loop {
+            //     iter.seek(bytes_of(&log_key));
+            //     if iter.valid().not() {
+            //         break;
+            //     }
+
+            //     log_key = match try_from_bytes(iter.key().unwrap()) {
+            //         Ok(k) => *k,
+            //         Err(_) => break,
+            //     };
+
+            //     let id = log_key.id();
+            //     assert!(id.0 > ReplicaId::ZERO);
+            //     assert!(id.1 > LocalInstanceId::ZERO);
+
+            //     assert_eq!(log_key.field(), InstanceFieldKey::FIELD_CMD);
+
+            //     log_key.set_field(field_status);
+
+            //     iter.seek(bytes_of(&log_key));
+            //     if iter.valid().not() {
+            //         break;
+            //     }
+
+            //     log_key = *from_bytes(iter.key().unwrap());
+            //     assert_eq!(log_key.id(), id);
+            //     assert_eq!(log_key.field(), field_status);
+
+            //     // TODO
+            // }
+
+            // iter.status()?;
+        }
+
         todo!()
     }
 }
