3ca8c3d6d8dfc6ff52c6599e7083742cce1d7e61
Nugine <nugine@foxmail.com>
2022-04-26 15:28:56 +0800

    server: run

--- crates/epkv-server/src/lib.rs
+++ crates/epkv-server/src/lib.rs
@@ -17,15 +17,32 @@ use self::config::Config;
 
 use epkv_rocks::data_db::DataDb;
 use epkv_rocks::log_db::LogDb;
+use epkv_utils::clone;
 
+use std::sync::atomic::{AtomicBool, Ordering::*};
 use std::sync::Arc;
 
 use anyhow::Result;
+use futures_util::pin_mut;
+use tokio::spawn;
 use tracing::debug;
+use wgp::WaitGroup;
 
 pub struct Server {
     log_db: Arc<LogDb>,
     data_db: Arc<DataDb>,
+    waiting_shutdown: AtomicBool,
+    waitgroup: WaitGroup,
+}
+
+impl Server {
+    fn is_waiting_shutdown(&self) -> bool {
+        self.waiting_shutdown.load(SeqCst)
+    }
+
+    fn set_waiting_shutdown(&self) {
+        self.waiting_shutdown.store(true, SeqCst);
+    }
 }
 
 impl Server {
@@ -38,8 +55,49 @@ impl Server {
 
         debug!("data_db is opened");
 
-        let server = Arc::new(Server { log_db, data_db });
+        let waiting_shutdown = AtomicBool::new(false);
+        let waitgroup = WaitGroup::new();
 
+        let server = Arc::new(Server { log_db, data_db, waiting_shutdown, waitgroup });
+
+        {
+            clone!(server);
+            spawn(server.serve_peer())
+        };
+
+        {
+            clone!(server);
+            spawn(server.serve_client());
+        };
+
+        {
+            let shutdown_signal: _ = tokio::signal::ctrl_c();
+            pin_mut!(shutdown_signal);
+            shutdown_signal.await?
+        }
+
+        {
+            server.set_waiting_shutdown();
+
+            let task_count = server.waitgroup.count();
+            debug!(?task_count, "waiting running tasks");
+            server.waitgroup.wait().await;
+        }
+
+        server.shutdown().await;
+
+        Ok(())
+    }
+
+    async fn serve_peer(self: Arc<Self>) {
+        todo!()
+    }
+
+    async fn serve_client(self: Arc<Self>) {
+        todo!()
+    }
+
+    async fn shutdown(self: Arc<Self>) {
         todo!()
     }
 }
