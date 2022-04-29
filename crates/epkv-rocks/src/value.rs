716ff6016b34c812d308c99a99c9a4d9e59dd630
Nugine <nugine@foxmail.com>
2022-04-29 23:12:32 +0800

    rocks: value

--- crates/epkv-rocks/src/value.rs
+++ crates/epkv-rocks/src/value.rs
@@ -2,7 +2,7 @@ use epkv_utils::bytes_str::BytesStr;
 
 use serde::{Deserialize, Serialize};
 
-#[derive(Clone, Deserialize, Serialize)]
+#[derive(Debug, Clone, Deserialize, Serialize)]
 pub enum Value {
     Str(BytesStr),
     U64(u64),
