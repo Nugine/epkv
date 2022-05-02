f1425ef28ba5d240b07d7d7565d6c0f210c8f866
Nugine <nugine@foxmail.com>
2022-05-02 10:16:12 +0800

    eval: rename from client

---
+++ crates/epkv-eval/src/lib.rs
@@ -0,0 +1,9 @@
+#![forbid(unsafe_code)]
+#![deny(
+    clippy::all,
+    clippy::as_conversions,
+    clippy::float_arithmetic,
+    clippy::integer_arithmetic,
+    clippy::must_use_candidate
+)]
+#![warn(clippy::todo, clippy::dbg_macro)]
