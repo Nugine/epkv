08e0eab1fe5debca647f84dc86628fbdb1873f55
Nugine <nugine@foxmail.com>
2022-05-04 21:39:55 +0800

    eval: bench: case3: latency_ms

--- crates/epkv-eval/src/bench.rs
+++ crates/epkv-eval/src/bench.rs
@@ -375,22 +375,22 @@ pub async fn case3(config: &Config, args: Case3) -> Result<serde_json::Value> {
         v
     };
 
-    #[allow(clippy::integer_arithmetic)]
+    #[allow(clippy::integer_arithmetic, clippy::float_arithmetic, clippy::as_conversions)]
     let latency = {
-        let min = latency_us.first().unwrap();
+        let min = latency_us.first().copied().unwrap();
         let q1 = latency_us[args.cmd_count / 4];
         let q2 = latency_us[args.cmd_count / 2];
         let q3 = latency_us[args.cmd_count * 3 / 4];
-        let max = latency_us.last().unwrap();
+        let max = latency_us.last().copied().unwrap();
         let p99 = latency_us[args.cmd_count * 99 / 100];
 
         json!({
-            "min": min,
-            "q1": q1,
-            "q2": q2,
-            "q3": q3,
-            "max": max,
-            "p99": p99,
+            "min": min as f64 / 1000.0, // ms
+            "q1": q1 as f64 / 1000.0, // ms
+            "q2": q2 as f64 / 1000.0, // ms
+            "q3": q3 as f64 / 1000.0, // ms
+            "max": max as f64 / 1000.0, // ms
+            "p99": p99 as f64 / 1000.0, // ms
         })
     };
 
