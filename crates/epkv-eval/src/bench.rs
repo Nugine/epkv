e2554501d702175d2762965b2ccd87e53e187c99
Nugine <nugine@foxmail.com>
2022-05-04 11:29:23 +0800

    eval: bench: case2

--- crates/epkv-eval/src/bench.rs
+++ crates/epkv-eval/src/bench.rs
@@ -27,9 +27,23 @@ pub struct Opt {
 #[derive(Debug, clap::Subcommand)]
 pub enum Command {
     Case1 {
+        #[clap(long)]
         key_size: usize,
+        #[clap(long)]
         value_size: usize,
+        #[clap(long)]
         cmd_count: usize,
+        #[clap(long)]
+        batch_size: usize,
+    },
+    Case2 {
+        #[clap(long)]
+        key_size: usize,
+        #[clap(long)]
+        value_size: usize,
+        #[clap(long)]
+        cmd_count: usize,
+        #[clap(long)]
         batch_size: usize,
     },
 }
@@ -54,6 +68,9 @@ pub async fn run(opt: Opt) -> Result<()> {
     match opt.cmd {
         Command::Case1 { key_size, value_size, cmd_count, batch_size } => {
             case1(&config, &opt.target, key_size, value_size, cmd_count, batch_size).await?
+        }
+        Command::Case2 { key_size, value_size, cmd_count, batch_size } => {
+            case2(&config, &opt.target, key_size, value_size, cmd_count, batch_size).await?
         }
     }
 
@@ -123,6 +140,82 @@ pub async fn case1(
         });
 
         let result_path = target_dir.join("case1.json");
+
+        let content = crate::pretty_json(&result)?;
+
+        println!("{}", content);
+
+        fs::write(&result_path, content)
+            .with_context(|| format!("failed to write result file {result_path}"))?;
+    }
+
+    Ok(())
+}
+
+pub async fn case2(
+    config: &Config,
+    target_dir: &Utf8Path,
+    key_size: usize,
+    value_size: usize,
+    cmd_count: usize,
+    batch_size: usize,
+) -> Result<()> {
+    #[allow(clippy::integer_arithmetic)]
+    {
+        ensure!(cmd_count % batch_size == 0);
+    }
+
+    let server = {
+        let first = config.servers.iter().next().unwrap();
+        let remote_addr = SocketAddr::from((first.1.ip, first.1.client_port));
+        let rpc_client_config = crate::default_rpc_client_config();
+        cs::Server::connect(remote_addr, &rpc_client_config).await?
+    };
+
+    let key = crate::random_bytes(key_size);
+    let value = crate::random_bytes(value_size);
+
+    server.set(cs::SetArgs { key: key.clone(), value: value.clone() }).await?;
+
+    let t0 = Instant::now();
+
+    for _ in 0..(cmd_count.wrapping_div(batch_size)) {
+        let futures: _ = (0..batch_size).map(|_| server.get(cs::GetArgs { key: key.clone() }));
+        let results: _ = join_all(futures).await;
+        for result in results {
+            result?;
+        }
+    }
+
+    let t1 = Instant::now();
+
+    drop(server);
+
+    #[allow(clippy::float_arithmetic)]
+    let time_ms = (t1 - t0).as_secs_f64() * 1000.0;
+
+    let cluster_metrics = {
+        let rpc_client_config = crate::default_rpc_client_config();
+        let mut map = BTreeMap::new();
+        for (name, c) in &config.servers {
+            let remote_addr = SocketAddr::from((c.ip, c.client_port));
+            let server = cs::Server::connect(remote_addr, &rpc_client_config).await?;
+            let metrics = server.get_metrics(cs::GetMetricsArgs {}).await?;
+            map.insert(name.to_owned(), metrics);
+
+            let output = server.get(cs::GetArgs { key: key.clone() }).await?;
+            assert_eq!(output.value.unwrap(), value);
+        }
+        map
+    };
+
+    {
+        let result = json!({
+            "time_ms": time_ms,
+            "cluster_metrics": cluster_metrics,
+        });
+
+        let result_path = target_dir.join("case2.json");
 
         let content = crate::pretty_json(&result)?;
 
