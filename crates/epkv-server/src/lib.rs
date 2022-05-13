680cd816fcb591b81983314aea5d97311445d7f9
Nugine <nugine@foxmail.com>
2022-05-13 16:45:10 +0800

    server: test

--- crates/epkv-server/src/lib.rs
+++ crates/epkv-server/src/lib.rs
@@ -496,7 +496,7 @@ mod tests {
 
     #[test]
     fn replica_future_size() {
-        assert_eq!(output_size(&EpkvReplica::handle_message), 424);
+        assert_eq!(output_size(&EpkvReplica::handle_message), 408);
         assert_eq!(output_size(&EpkvReplica::run_propose), 376);
     }
 
